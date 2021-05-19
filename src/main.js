b18445820b21d492fe06ff2fd851a7c042e47de8
Kalys01 <kalysosmon96.g@gmail.com>
2021-05-20 01:10:13 +0600

    installed firebase and configured

--- src/main.js
+++ src/main.js
@@ -5,13 +5,16 @@ import store from './store'
 import Vuelidate from 'vuelidate'
 import dateFilter from '@/filters/date.filter'
 // import i18n from './plugins/i18n'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-var firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyBlgh5vDLt0yDXN64fB5XqAkGn257SAv3Q",
   authDomain: "client-authorization.firebaseapp.com",
   projectId: "client-authorization",
@@ -19,11 +22,17 @@ var firebaseConfig = {
   messagingSenderId: "636974089077",
   appId: "1:636974089077:web:25521cac2841b881c6c93c",
   measurementId: "G-2YFRS9YZ5W"
-};
+})
 
-new Vue({
-  router,
-  store,
-  // i18n,
-  render: h => h(App)
-}).$mount('#app')
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if(!app) {
+    app = new Vue({
+      router,
+      store,
+      // i18n,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
